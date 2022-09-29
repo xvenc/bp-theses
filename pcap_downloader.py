28d7fc9204128f027677a074edbc6bdc29548523
Vaclav Korvas <korvasv@seznam.cz>
2022-09-29 20:11:58 +0200

    Add file to download pcap based on generated .csv file

---
+++ pcap_downloader.py
@@ -0,0 +1,25 @@
+from requests import Session
+from os import path, mkdir
+
+
+class Downloader:
+
+    def __init__(self, url : str, auth_token : str):
+        self.url = url
+        self.token = auth_token
+
+    def _download_pcap(self, sample_id, taksk_id, outpud_dir, filename):
+        s = Session()
+        headers = {'Authorization': 'Bearer {0}'.format(self.token)}
+        data = s.get(url= self. url + f"{sample_id}/{taksk_id}/dump.pcap",headers=headers).content
+        if not path.isdir(outpud_dir):
+            mkdir(outpud_dir)
+        with open("%s/%s.pcap" % (outpud_dir,path.splitext(filename)[0]), "wb") as wf:
+                    wf.write(data)
+        return True
+
+    def download(self, csv, task_id, outpud_dir):
+        for row in csv:
+            res = self._download_pcap(row['Sample_id'], task_id, outpud_dir, row['Filename'])
+            if res:
+                print("Pcap for {0} downloaded.".format(row['Filename']))
--- triage_client.py
+++ triage_client.py
@@ -3,24 +3,34 @@ from os import path, listdir
 import sys
 import getopt
 from hashlib import md5
+from pcap_downloader import Downloader
+import csv
 
 public_api = "https://api.tria.ge/"
 auth_api_key = "349a1f88ad1e2aee63e6e304a1400ca1af82e423"
 
 
 def help():
-    print("Usage: python3 triage_client [OPTION]\n")
-    print("Options:")
+    print("Usage: python3 triage_client [COMMAND] [OPTION]\n")
+    print("Command:")
     print("  --help\tShow this help message and exits.")
-    print("  -d\tSpecifies directory with malware samples.")
-    print("  -f\tSpecifies one malware sample.")
-    print("  -p\tSets password for zip/tar protected files.")
+    print("  --submit\tSubmit file or whole directory to tria.ge")
+    print("\n  Submit options:")
+    print("\t-d\tSpecifies directory with malware samples.")
+    print("\t-f\tSpecifies one malware sample.")
+    print("\t-p\tSets password for zip/tar protected files.")
+    print("\n  --download\tDownload all files from specified .csv file")
+    print("\n  Download options:")
+    print("\t-f\tSpecifies one .csv file with informations about samples.")
+    print('\t-d\tSpecifies output directory name for dowloaded pcaps')
 
 # parse command line arguments
-def arg_parse() -> dict:
-    arguments = {'--help' : False, '-d' : [False,""], '-f' : [False,""], '-p' : [False, ""]}
+def arg_parse():
+    command = {'--submit' : False, '--download' : False}
+    option = {'-d' : [False,""], '-f' : [False,""], '-p' : [False, ""],
+                 '-o': [False, ""]}
     try:
-        options, args = getopt.getopt(sys.argv[1:], "d:f:p:", ["help"])
+        options, args = getopt.getopt(sys.argv[1:], "d:f:p:o:", ["help", "submit", "download"])
     except:
         help()
         sys.exit(1)
@@ -30,15 +40,16 @@ def arg_parse() -> dict:
         if opt == '--help':
             help()
             exit(0)
-        else:
-            arguments[opt][0] = True
-            arguments[opt][1] = arg
+        elif opt in option:
+            option[opt][0] = True
+            option[opt][1] = arg
+        elif opt in command:
+            command[opt] = True
 
-    # MBY TODO multiple files set
-    if arguments["-f"][0] and not arguments['-d'][0]:
-        pass
+    if command['--submit'] and command['--download']:
+        sys.exit(1)
 
-    return arguments
+    return command,option
 
 # return md5 hash of the submited sample
 def get_hash(sample_id: str) -> str:
@@ -75,13 +86,37 @@ def submit_directory(directory, client : triage.Client):
 # MAIN
 
 client = triage.Client(auth_api_key, public_api)
-args = arg_parse()
+command,option = arg_parse()
 
-if args['-d'][0] and path.isdir(args["-d"][1]):
-    submit_directory(args["-d"][1], client)
+if command['--submit']:
+    print("submit")
+    if option['-d'][0] and path.isdir(option["-d"][1]):
+        submit_directory(option["-d"][1], client)
 
-elif args['-f'][0] and path.isfile(args['-f'][1]):
-    res = submit_file(args["-f"][1])
-    print(res)
+    elif option['-f'][0] and path.isfile(option['-f'][1]):
+        res = submit_file(option["-f"][1])
+        print(res)
 
+elif command['--download']:
+    #Pcap file downloader
+    d = Downloader(public_api+"v0/samples", auth_api_key)
+    with open(option["-f"][1],mode='r') as csv_file:
+        content = csv.DictReader(csv_file)
+        d.download(content,'behavioral1', option['-o'][1])
 
+# List all owned_samples
+#for r in client.owned_samples():
+#    print("filename: {0} id: {1}".format(r['filename'], r['id']))
+#
+## Logging
+#with open('logs/test.csv', 'w') as f:
+#    # create the csv writer
+#    writer = csv.writer(f)
+#
+#    header = ['Filename', 'Sample_id', 'mb5_hash']
+#    data = ['KiffAppE2.bin', '220928-zc965aabdn', 'db5cc5204a082888533280e4cb9099b0']
+#    data2 = ['220925-wx71qaghdr_pw_infected.zip', '220927-yhag1sfdgr', '66b39f02f8aab03e7d6b0cdc63eb2718']
+#    # write a row to the csv file
+#    writer.writerow(header)
+#    writer.writerow(data)
+#    writer.writerow(data2)
