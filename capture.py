a552521dfa6f4c663158428f1725a0c977b2ecea
Vaclav Korvas <korvasv@seznam.cz>
2023-02-25 11:42:56 +0100

    Add function to handle sigint interupt signal

--- capture.py
+++ capture.py
@@ -18,7 +18,7 @@ def argparse():
     try:
         options, args = getopt.getopt(sys.argv[1:], "d:m:t:", ["help", "live"])
     except:
-    #    help()
+        # help()
         print("Error")
         sys.exit(1)
 
@@ -40,31 +40,34 @@ def argparse():
 
 # Function to handle SIGINT and exit with 0 and print overall statistics
 def handler(signum, frame):
+    print("\n")
     classifier.score()
     sys.exit(0)
 
 # MAIN
 args, cmds = argparse()
 extractor = Extractor()
+extractor.extract(args, args['-t'][1]) # Extract ioc's from the report files
 classifier = Classifier(extractor.ioc_map, extractor.ioc_cnt)
 signal.signal(signal.SIGINT, handler)
 
-extractor.extract(args, args['-t'][1])
-classifier.init_counter()
+classifier.init_counter() # Init counters for each family
 
 # TODO live capture and score board
 # If in common.txt than 1 point, if in infected than 5 points, other 2 points
 while True and cmds['--live']:
     # Classify flows, http, dns and tls
-    classifier.classify(os.path.join(suricata_log, "eve-nsm.json"))
-    classifier.classify(os.path.join(suricata_log, "eve-flow.json"))
-
+    # classifier.classify(os.path.join(suricata_log, "eve-nsm.json"))
+    # classifier.classify(os.path.join(suricata_log, "eve-flow.json"))
+    classifier.live_capture(os.path.join(suricata_log, "eve-nsm.json"))
     # TODO print found IOC's and mby clear the read file
 
-    time.sleep(10)
+    time.sleep(4)
 
 # Print statistics about families and found IOC's
 if args['-m'][0]:
     extractor.ioc_spec_print(args['-m'][1], True)
+elif args['-t'][0]:
+    extractor.only_iocs()
 else:
     extractor.ioc_print()
